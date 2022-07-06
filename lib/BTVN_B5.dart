ae8b0c3a2c912c0e7bd0f95eec9f2becdc5b7b36
vietanh <vietanh030498@gmail.com>
2022-07-06 11:09:00 +0700

    add btvn 5

--- lib/BTVN_B5.dart
+++ lib/BTVN_B5.dart
@@ -59,7 +59,38 @@ class Home extends StatefulWidget {
   _productState createState() => _productState();
 }
 
+class _miniState extends State {
+  //_miniState();
+  //final linkName;
+  //final price;
+  //final title;
+  //final des;
+  //final text = 'haha';
+  var _count = 0;
+  void _setState() {
+    setState(() => _count++);
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            '$_count',
+          ),
+          ElevatedButton(
+            onPressed: _setState,
+            child: Text('haha'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class _productState extends State<Home> {
+  final String l = 'text';
+  _miniState cr() => _miniState();
   void onTap(String l, String t, double p, String s) {
     Navigator.push(
         context,
@@ -69,18 +100,40 @@ class _productState extends State<Home> {
   }
 
   Widget build(BuildContext context) {
+    Widget productTitle(String title) {
+      return Container(
+        height: 50,
+        color: Color(0xFF0E3311).withOpacity(0.5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.heart_broken, color: Colors.white),
+            Text(
+              title,
+              style: TextStyle(color: Colors.white),
+            ),
+            Icon(Icons.shopping_cart, color: Colors.white),
+          ],
+        ),
+      );
+    }
+
     Widget products(
         String linkName, String title, double price, String description) {
       return InkWell(
         onTap: () => onTap(linkName, title, price, description),
-        child: Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: Image.asset(
-            'assets/images/$linkName.jpg',
-            fit: BoxFit.contain,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset('assets/images/$linkName.jpg', fit: BoxFit.fill),
+              Positioned(
+                width: MediaQuery.of(context).size.width / 2,
+                bottom: 0,
+                child: productTitle(title),
+              )
+            ],
           ),
         ),
       );
@@ -95,27 +148,34 @@ class _productState extends State<Home> {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.menu),
         centerTitle: true,
+        actions: [Icon(Icons.more_vert), Icon(Icons.shopping_cart)],
         title: Text(
-          'Products',
+          'Viet Anh',
           style: TextStyle(fontSize: 30, color: Colors.red[500]),
         ),
       ),
-      body: GridView.count(mainAxisSpacing: 0, crossAxisCount: 2, children: [
-        //products('1', 'Ảnh 1', 23, 'Của mọi nhà'),
-        //products('2', 'Ảnh 2', 34, 'Của mọi nhà'),
-        //products('3', 'Ảnh 3', 23, 'Của mọi nhà'),
-        Image.asset(
-          'assets/images/1.png',
-        ),
-        Image.asset(
-          'assets/images/1.png',
-        ),
-
-        Image.asset(
-          'assets/images/1.png',
-        ),
-      ]),
+      body: GridView.count(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: [
+            products('1', 'Ảnh 1', 23, 'Của mọi nhà'),
+            products('2', 'Ảnh 2', 34, 'Của mọi nhà'),
+            products('3', 'Ảnh 3', 23, 'Của mọi nhà'),
+            //cr().build(context),
+            // Container(
+            //     decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.all(Radius.circular(30)),
+            //         image: DecorationImage(
+            //             image: AssetImage('assets/images/1.png'), fit: BoxFit.fill))
+            // child: Image.asset(
+            //   'assets/images/1.png',
+            //   fit: BoxFit.fill,
+            // ),
+          ]),
     );
   }
 }
